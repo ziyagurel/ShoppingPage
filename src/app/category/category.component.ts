0e39bf62a442c331e26f4dced5e058a93a5addc7
ziyagurel <ziyagurel55@hotmail.com>
2020-05-29 21:25:14 +0300

    Category and Products added to list with ngFor

--- src/app/category/category.component.ts
+++ src/app/category/category.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Category } from './category';
 
 @Component({
   selector: 'app-category',
@@ -8,7 +9,14 @@ import { Component, OnInit } from '@angular/core';
 export class CategoryComponent implements OnInit {
 
   constructor() { }
-
+  title = "Kategoriler"
+  categories : Category[] = [
+    {id:1, name:"Bilgisayar"},
+    {id:2, name:"Bilgisayar Yazılım"},
+    {id:3, name:"Bilgisayar Donanım"},
+    {id:4, name:"Kitap"},
+    {id:5, name:"Kulaklık"}
+  ]
   ngOnInit(): void {
   }
 
