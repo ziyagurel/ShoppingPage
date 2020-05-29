41f733c9bf64a55651dcc0f24d5fba83d0887b04
ziyagurel <ziyagurel55@hotmail.com>
2020-05-29 21:36:38 +0300

    Piple application. Price ₺ was made.

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -12,7 +12,7 @@ export class ProductComponent implements OnInit {
   title = "Ürünler"
   products : Product[] =[
     {id:1, name: "Laptop", price:4500, categoryId: 1, description: "ASUS X543MA CELERON N4000 1.1GHZ-4GB RAM-500GB HDD-15.6-W10 NOTEBOOK", imageUrl: "https://images.unsplash.com/photo-1566476927456-446189d7b1ca?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"},
-    {id:2, name: "Mause", price:45, categoryId: 2, description: "LOGITECH M171 MOUSE BLACK", imageUrl: "https://images.unsplash.com/photo-1566476927456-446189d7b1ca?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"}
+    {id:2, name: "Mause", price:45, categoryId: 2, description: "LOGITECH M171 MOUSE BLACK for ASUS X543MA CELERON N4000", imageUrl: "https://images.unsplash.com/photo-1566476927456-446189d7b1ca?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"}
   ]
   ngOnInit(): void {
   }
