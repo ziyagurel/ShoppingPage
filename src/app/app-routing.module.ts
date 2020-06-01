f9faa0155957d1db3258e3d3577b273af7d7f1b7
ziyagurel <ziyagurel55@hotmail.com>
2020-06-01 15:12:01 +0300

    Using Routing operation

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { ProductComponent } from './product/product.component';
+
+
+const routes: Routes = [
+  {path : 'products', component : ProductComponent},
+  {path : '', redirectTo : 'products', pathMatch : 'full'}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 import { FormsModule, FormArray} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http'
 
+import { AppRoutingModule } from "./app-routing.module"
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { CategoryComponent } from './category/category.component';
 import { ProductComponent } from './product/product.component';
 import { ProductFilterPipe } from './product/product-filter.pipe';
 import { AlertifyService } from './services/alertify.service';
+import { from } from 'rxjs';
 
 @NgModule({
   declarations: [
@@ -21,7 +23,8 @@ import { AlertifyService } from './services/alertify.service';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    AppRoutingModule
   ],
   //providers içerisine eklenenle global servis olduğunu gösterir.
   providers: [AlertifyService],
