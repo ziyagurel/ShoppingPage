55c33fce16a6f1c7a8b17a2cfa40fb56e5dfd12a
ziyagurel <ziyagurel55@hotmail.com>
2020-06-01 21:03:18 +0300

    path url update

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -12,8 +12,12 @@ export class ProductService {
   path = "http://localhost:3000/products";
 
   getProductList(categoryId):Observable<Product[]>{
+    let newPath = this.path;
+    if(categoryId){
+      newPath += "?categoryId=" + categoryId
+    }
     return this.http
-    .get<Product[]>(this.path + "?categoryId=" + categoryId).pipe(
+    .get<Product[]>(newPath).pipe(
       tap(data=>console.log(JSON.stringify(data))),
       catchError(this.handleError)
     )
